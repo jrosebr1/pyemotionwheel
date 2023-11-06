c63880dc3a17e0e4b892381f1d6328ae2755a7ff
Adrian Rosebrock <jrosebr1@umbc.edu>
2023-11-06 09:29:37 -0500

    Made EmotionNode PEP8

--- pyemotionwheel/emotion_node.py
+++ pyemotionwheel/emotion_node.py
@@ -3,14 +3,49 @@ from anytree import AnyNode
 
 
 class EmotionNode(AnyNode):
+    """
+    A class representing a node in an EmotionTree, extending the AnyNode
+    class.
+
+    This node is used to represent an emotion in a hierarchical structure,
+    with the possibility of having parent-child relationships to represent
+    complex emotional compositions.
+
+    Attributes:
+        name (str): The human-readable name of the emotion.
+
+    Args:
+        **kwargs: Arbitrary keyword arguments passed to the parent class
+            constructor.
+    """
 
     # provide a type hint for the name of the node
     name: str
 
     def __init__(self, **kwargs):
-        # call teh parent constructor
+        """
+        Initializes a new instance of EmotionNode.
+
+        This constructor initializes the EmotionNode with the provided keyword
+        arguments. The `name` of the node (i.e., the human-readable name of
+        the emotion) is expected to be among the `kwargs`.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments. The `name` keyword is
+                expected to be in this list of arguments.
+        """
+        # call the parent constructor
         super().__init__(**kwargs)
 
     def __str__(self):
+        """
+        Return the human-readable string representation of the EmotionNode.
+
+        The string representation of an EmotionNode is its name (i.e., amused,
+        delighted, angry, agitated, etc.).
+
+        Returns:
+            str: The name of the emotion.
+        """
         # return the name of the emotion as the string representation
         return self.name
